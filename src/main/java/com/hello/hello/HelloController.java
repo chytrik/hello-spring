27cfad16dfff035f77854c2199a4a54864360807
Patrik Plachy <chytrik@gmail.com>
2022-06-17 13:34:54 +0200

    next

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
-    String greeting = "Hello World!";
+    String greeting = "Hello World 1!";
 
     @RequestMapping("/")
     public String index() {
